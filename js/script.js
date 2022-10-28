4043e736dbc06c6107d0f5a82c4babaad62449c2
Vinicius <vinicardoso216@gmail.com>
2022-10-28 10:34:05 -0300

    refactor: Another way to make Smooth Scrolling

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const currentYear = new Date().getFullYear();
 
 yearEl.textContent = currentYear;
 
+
 // Make mobile navigation work
 const btnNavEl = document.querySelector('.btn-mobile-nav');
 const headerEl = document.querySelector('.header');
@@ -11,3 +12,26 @@ const headerEl = document.querySelector('.header');
 btnNavEl.addEventListener('click', function() {
     headerEl.classList.toggle('nav-open');
 });
+
+
+// Smooth scrolling animation
+const allLinks = document.querySelectorAll('a:link');
+
+allLinks.forEach(function(link) {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const href = link.getAttribute('href');
+
+        // Scroll back to top
+        if (href === "#") window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+
+        // Scroll to other links
+        if (href !== "#" && href.startsWith('#')) {
+            const sectionEl = document.querySelector(href);
+            sectionEl.scrollIntoView({behavior: 'smooth'});
+        }
+    })
+})
